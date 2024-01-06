f7153250fa028ac974a46cc1c5c92bb5bc609156
orzarhi <or.zarhi11@gmail.com>
2024-01-07 00:01:46 +0200

    Add new logo and company logo placeholder images

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,14 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { formatDistanceToNowStrict } from 'date-fns'
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
 
+export const formatMoney = (amount: number) =>
+    new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    }).format(amount)
+
+export const relativeDate = (from: Date) =>
+    formatDistanceToNowStrict(from, { addSuffix: true })
