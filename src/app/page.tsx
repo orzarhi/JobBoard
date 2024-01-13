657a6a37b0978e125a1070cdeb807935172e1fa9
orzarhi <or.zarhi11@gmail.com>
2024-01-13 23:30:11 +0200

    Add Navbar and Footer components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { JobFilterSidebar } from '@/components/JobFilterSidebar'
 import { JobResults } from '@/components/JobResults'
 import { H1 } from '@/components/ui/h1'
 import { JobFilterValues } from '@/lib/validation/jobFilter'
+import { Metadata } from 'next'
 
 interface HomeProps {
   searchParams: {
@@ -9,6 +10,31 @@ interface HomeProps {
     type?: string,
     location?: string,
     remote?: string,
+  }
+}
+
+const getTitle = ({ q, type, location, remote }: JobFilterValues) => {
+  const titlePrefix = q
+    ? `${q} jobs`
+    : type
+      ? `${type} developer jobs`
+      : remote
+        ? 'Remote developer jobs'
+        : 'All developer jobs'
+
+  const titleSuffix = location ? ` in ${location}` : ''
+
+  return `${titlePrefix}${titleSuffix}`
+}
+
+export const generateMetadata = ({ searchParams: { q, type, location, remote } }: HomeProps): Metadata => {
+  return {
+    title: `${getTitle({
+      q,
+      type,
+      location,
+      remote: remote === 'true',
+    })} | Flow Jobs`
   }
 }
 
@@ -24,9 +50,7 @@ export default async function Home({ searchParams: { q, type, location, remote }
   return (
     <main className='max-w-5xl m-auto px-3 my-10 space-y-10'>
       <div className='space-y-5 text-center'>
-        <H1>
-          Developer jobs
-        </H1>
+        <H1>{getTitle(filterValues)}</H1>
         <p className='text-muted-foreground'>Find your dream job.</p>
       </div>
       <section className='flex flex-col md:flex-row gap-4'>
